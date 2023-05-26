285c30d178a2a43c7ab0d9176dcc14f85bd70655
Yugal <yugalgarg7@gmail.com>
2023-05-26 19:23:35 +0530

    Exercise 1.9

--- part1_unicafe/src/App.js
+++ part1_unicafe/src/App.js
@@ -2,6 +2,26 @@ import { useState } from "react"
 
 const Header = (props) => <h1>{props.text}</h1>
 
+const Statistics = (props) => {
+    if(props.value[3]===0){
+        return(
+            <>
+            <p>No feedback given</p>
+            </>
+        )
+    }
+    return(
+        <>
+        <p>{props.text[0]}: {props.value[0]}</p>
+        <p>{props.text[1]}: {props.value[1]}</p>
+        <p>{props.text[2]}: {props.value[2]}</p>
+        <p>{props.text[3]}: {props.value[3]}</p>
+        <p>{props.text[4]}: {props.value[4]}</p>
+        <p>{props.text[5]}: {props.value[5]}</p>
+        </>
+    )
+}
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
@@ -46,12 +66,7 @@ const App = () => {
         <button onClick={handleNeutral}>Neutral</button>
         <button onClick={handleBad}>Bad</button>
         <Header text='Statistics' />
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>All: {total} </p>
-        <p>Average: {average}</p>
-        <p>Positive: {positive}%</p>
+        <Statistics text={['Good', 'Bad', 'Neutral', 'All', 'Positive', 'Average']} value={[good, bad, neutral, total, positive, average]} />
         </>
     )
 }
